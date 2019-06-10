3427385ba7236c13e3dee3f5e5c95769ca302357
Marek Ciesla <ciesla.marek@gmail.com>
2019-06-10 15:38:54 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log('siema');
+const name = 'Marek';
+let isTrue = true;
+isTrue = false;
+isTrue = 'Tak';
+console.log(name);
+console.log(isTrue);
+console.log(`Mam na imie ${name} i mówie ${isTrue}`);
