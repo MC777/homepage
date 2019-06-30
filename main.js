5bfe30882c65a5c8f8a86356be6797f86513c917
Marek Ciesla <ciesla.marek@gmail.com>
2019-06-30 23:55:51 +0200

    Added handleClick function

--- main.js
+++ main.js
@@ -46,3 +46,18 @@ const welcomeMessage = (x,y) => {
 }
 
 console.log(welcomeMessage(name, surname))
+
+
+function handleClick() {
+  console.log('halo');
+}
+
+const button = document.querySelector('.header__button--js');
+console.log(button);
+button.addEventListener('click', (e) => {
+  const header = document.querySelector('.header__title--js');
+  header.innerHTML = 'klik klik';
+  alert('hello');
+  // console.log(e);
+  // console.log('Hello arrow!');
+});
