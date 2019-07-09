de1109a4db8d43e7efd47401fd2ad8cec9807700
Marek Ciesla <ciesla.marek@gmail.com>
2019-07-09 23:46:31 +0200

    Added object.js file

---
+++ object.js
@@ -0,0 +1,10 @@
+const deathStar = {
+  diameter: 120000,
+  fire: (targer) => {
+    return console.log(`${target} destroyed`)
+  },
+  isOperating: true,
+  levels: 357,
+  name: 'Death Star',
+  population: 1000,
+}
